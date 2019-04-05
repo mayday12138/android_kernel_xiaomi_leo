031f412574e5eb19a1d65215dd7874a69b858fe2
Gah0 <1174012692@qq.com>
2019-04-05 14:05:34 +0800

    update build.sh and strip.sh

--- libra.sh
+++ libra.sh
@@ -1,5 +1,7 @@
 export ARCH=arm64
 export SUBARCH=arm64
-export CROSS_COMPILE=/home/gah0/gcc/gcc-linaro-5.4.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=/home/gah0/gcc/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 make libra_defconfig
 make -j32
+
+/home/gah0/gcc/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- --strip-unneeded /home/gah0/android_kernel_xiaomi_leo/drivers/staging/qcacld-2.0/wlan.ko
--- strip.sh
+++ strip.sh
@@ -1,6 +1,6 @@
 mkdir /home/gah0/桌面/Xiaomi_Kernel_OpenSource/modules
 find . -name *.ko | xargs cp -a --target-directory=/home/gah0/桌面/Xiaomi_Kernel_OpenSource/modules/ &> /dev/null
-/data/aarch64-linux-android-4.9/bin/aarch64-linux-android-strip --strip-unneeded wlan.ko
+/home/gah0/gcc/gcc-linaro-5.4.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- --strip-unneeded wlan.ko
 
 
 
